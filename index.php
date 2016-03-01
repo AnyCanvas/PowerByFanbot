3de448604ca989e565def7c2b0b2bb2cac0d540b
Pedro <pedrocch@fanbot.me>
2016-03-01 11:19:39 -0600

    *

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<?php
-	session_start();
-	include 'resources/library/functions.php'; 
+<?php $actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']; ?>
 
-	if( isset($_GET["name"]) ){
-			$_SESSION['fnbt']['name'] = $_GET["name"];
-	} else{
-		if(isset($_SESSION['fnbt']['name'])){
-			unset($_SESSION['fnbt']['name']);
-		}
-	}
-	$_SESSION['page'] = 0;
-	?>
-<!DOCTYPE html>
-<html>
-<head>
-<?php require_once( "resources/html/header.php" ); ?>
-	<title>Bienvenido a Fanbot</title>
-</head>
 
-<?php require_once( "resources/html/index.php" ); ?>
 
-</html>
+<script> alert('<?php echo $actual_link; ?>');</script>
