1234fbf73e3778c5723da3961f9c9b4772963575
Pedro <pedrocch@fanbot.me>
2016-03-02 17:27:56 -0600

    *

--- resources/html/login.php
+++ resources/html/login.php
@@ -15,36 +15,6 @@
 		}	
 	</script>
 
-<?php	
-	
-	if(isset($_SESSION['fnbt']['name'])){
-		if ($_SESSION['fnbt']['name'] == 'centi'){
-			$centi = true;
-		} else {
-			$centi = false;
-		}
-	} else {
-		$centi = false;
-	}
-	
-	
-	
-	if ( $centi ){ ?>
-
-	<div class="container-fluid">		
-		<div id="upper-div" style="height: 74vh; width: 100vw; background-color: #004680;">
-		<div class="div-wrapper full " style="background-image: url(./media/clients/centinela/clasicoshot.png); background-repeat: no-repeat; background-position: center center; background-size: 100% auto;">
-		</div>
-		</div>
-	    <footer style="height: 24vh; width: 100vw;">
-		      <p class="fnbt-name-text grey-text">Es tu primera ves con aqui<br>¡Disfruta la experiencia!<p>
-			  <a class="waves-effect waves-light btn fb-btn btn-centered" style=" background-color: #405A9F; font-size: 3vw;" onclick="postclick();"><i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Continuar con Facebook</a>
-	    </footer>
-
-	</div>
-	
-<?php	} else { ?>
-
 	<div class="container-fluid">		
 		<div id="upper-div" style="height: 74vh; width: 100vw;" class="blue">
 		<div class="div-wrapper full login-img" style="background-image: url(./media/clients/<?php echo $_SESSION['site']['dir'] ?>/login.png); background-repeat: no-repeat; background-position: center center; background-size: 100% auto;">
@@ -58,7 +28,6 @@
 
 	</div>
 
-<?php } ?> 
 	<script>
 			var browserAgent = navigator.userAgent;
 		console.log(browserAgent);
