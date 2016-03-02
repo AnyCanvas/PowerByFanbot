460c83abd7ddc5c7a8efb24ebdb953ae72fe075b
Pedro <pedrocch@fanbot.me>
2016-03-02 17:33:25 -0600

    *

--- resources/html/error2.php
+++ resources/html/error2.php
@@ -7,7 +7,7 @@
     <body>
 
 	<div class="container-fluid red accent-2" style="height: 100%; width: 100%" >
-		<div id="upper-div" class="red accent-2" style=" height: 75%; background-image: url(./images/error2.png); background-repeat: no-repeat; background-position: center bottom; background-size: auto 60%;">
+		<div id="upper-div" class="red accent-2" style=" height: 75%; background-image: url(/images/clients/<?php echo $_SESSION['site']['dir'] ?>/error2.png); background-repeat: no-repeat; background-position: center bottom; background-size: auto 60%;">
 		</div>
 	    <footer style="height: 25%;" class="red accent-2">
 		      <p class="error-text white-text">Esta Fanbot está desconectada,<br>puedes probar Fanbot en otro negocio.</p>
--- resources/html/error3.php
+++ resources/html/error3.php
@@ -6,7 +6,7 @@
    <body>
 
 	<div class="container-fluid red accent-2" style="height: 100%; width: 100%" >
-		<div id="upper-div" class="red accent-2" style=" height: 75%; background-image: url(./images/error3.png); background-repeat: no-repeat; background-position: center bottom; background-size: auto 60%;">
+		<div id="upper-div" class="red accent-2" style=" height: 75%; background-image: url(/images/clients/<?php echo $_SESSION['site']['dir'] ?>/error3.png); background-repeat: no-repeat; background-position: center bottom; background-size: auto 60%;">
 		</div>
 	    <footer style="height: 25%;" class="red accent-2">
 		      <p class="error-text white-text">Esta Fanbot está suspendida,<br>puedes probar Fanbot en otro negocio.</p>
