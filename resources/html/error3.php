ad513d33877e64003f4ed0034dbbfdbb288d135d
Pedro <pedrocch@fanbot.me>
2016-03-03 10:54:22 -0600

    *

--- resources/html/error3.php
+++ resources/html/error3.php
@@ -8,7 +8,7 @@
 	<div class="container-fluid red accent-2" style="height: 100%; width: 100%" >
 		<div id="upper-div" style=" height: 75%; background-image: url(/media/clients/<?php echo $_SESSION['site']['dir'] ?>/error1.png); background-repeat: no-repeat; background-position: center bottom; background-size: auto 60%; background-color: <?php echo $_SESSION['fnbt']['config']["bgcolor"] ?>;">
 		</div>
-	    <footer style="height: 25%;" style="background-color: <?php echo $_SESSION['fnbt']['config']["bgcolor"] ?>;">
+	    <footer style="height: 25%; background-color: <?php echo $_SESSION['fnbt']['config']["bgcolor"] ?>;">
 
 	    </footer>
 
