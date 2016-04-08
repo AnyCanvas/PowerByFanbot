d07fb4172a3bd210c40d5143dd6ed631aa7ca0fc
Pedro <pedrocch@fanbot.me>
2016-04-08 13:19:19 -0500

    *

--- resources/actions/facebook/like.php
+++ resources/actions/facebook/like.php
@@ -44,9 +44,8 @@
 				<div class="cssload-loader btn-centered" style="z-index: 10; top: 45vh; margin: auto;"></div>
 			</div>
 		</div>
-		<div id="upper-div" style="height: 75%; background-color: <?php echo $_SESSION['site']['bgcolor'] ?>;">
+		<div id="upper-div" style="height: 75%; background-color: <?php echo $_SESSION['site']['bgcolor'] ?>; background-image: url(./media/clients/<?php echo $_SESSION['site']['dir'] ?>/like.png); background-repeat: no-repeat; background-position: center center; background-size: 100% auto;">
 		<div class="div-wrapper full" style=" background-color: <?php echo $_SESSION['site']['bgcolor'] ?>;">
-			<img class="center-img fbpage-img" src="/media/clients/<?php echo $_SESSION['site']['dir'] ?>/like.png" class="img-responsive img-thumbnail center-block" alt="Cinque Terre">
 		</div>
 		</div>
 	    <footer style="height: 25%;">
