5c224b190c1a2007095e4e17379eecea7fb72136
Pedro <pedrocch@fanbot.me>
2016-04-08 13:40:38 -0500

    *

--- resources/html/final/classic.php
+++ resources/html/final/classic.php
@@ -9,13 +9,9 @@
     <body>
 
 	<div class="container-fluid" style="height: 100%; width: 100%">
-		<div id="upper-div" style="height: 75%; background-color: <?php echo $_SESSION['site']['bgcolor'] ?>; background-image: url(./media/clients/<?php echo $_SESSION['site']['dir'] ?>/like.png); background-repeat: no-repeat; background-position: center center; background-size: 100% auto;">
+		<div id="upper-div" style="height: 75%; background-color: <?php echo $_SESSION['site']['bgcolor'] ?>; background-image: url(./media/clients/<?php echo $_SESSION['site']['dir'] ?>/success.png); background-repeat: no-repeat; background-position: center center; background-size: 100% auto;">
 		</div>
 	    <footer style="height: 25%; padding-top: 17.5vh;" style="background-color: <?php echo $_SESSION['site']['bgcolor'] ?>;">
-<!--		    <div class="btn-full-div">
-			  <a class="waves-effect waves-light btn white black-text btn-full teal accent-2" href="http://fanbot.me/">
-			<i class="material-icons left">arrow_forward</i>CONOCE FANBOT</a>
-		    </div> -->
 			  <a class="waves-effect waves-light btn white-text btn-centered red darken-3"><i class="material-icons left">arrow_forward</i>SOY EL ENCARGADO</a>
 
 	    </footer>
