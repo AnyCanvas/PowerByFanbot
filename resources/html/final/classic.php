10647d0fcc3b7c69207df4460f3caa243ba60979
Pedro <pedrocch@fanbot.me>
2016-04-11 10:28:10 -0500

    *

--- resources/html/final/classic.php
+++ resources/html/final/classic.php
@@ -6,12 +6,10 @@
     <body>
 
 	<div class="container-fluid" style="height: 100%; width: 100%">
-		<div id="upper-div" style="height: 75%; background-color: <?php echo $_SESSION['site']['bgcolor'] ?>; background-image: url(./media/clients/<?php echo $_SESSION['site']['dir'] ?>/success.png); background-repeat: no-repeat; background-position: center center; background-size: 100% auto;">
-		</div>
-	    <footer style="height: 25%; padding-top: 17.5vh;" style="background-color: <?php echo $_SESSION['site']['bgcolor'] ?>;">
-			  <a class="waves-effect waves-light btn white-text btn-centered red darken-3"><i class="material-icons left">arrow_forward</i>SOY EL ENCARGADO</a>
-	    </footer>
+		<div id="upper-div" style="height: 75%; background-color: <?php echo $_SESSION['site']['bgcolor'] ?>; background-image: url(./media/clients/<?php echo $_SESSION['site']['dir'] ?>/success.png); background-repeat: no-repeat; background-position: center top; background-size: 100% auto;">
 
+	    <a class="waves-effect waves-light btn white-text btn-centered red darken-3"><i class="material-icons left">arrow_forward</i>SOY EL ENCARGADO</a>
+		</div>
 	</div>
 
 	<script>
