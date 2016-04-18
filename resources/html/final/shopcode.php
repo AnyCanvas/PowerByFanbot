01228da1b3c26aa3127701198d7df65391ca6642
Pedro <pedrocch@fanbot.me>
2016-04-18 10:36:24 -0500

    *

--- resources/html/final/shopcode.php
+++ resources/html/final/shopcode.php
@@ -5,7 +5,7 @@
 	    showModal(){
 		  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 		  $('#modalShop').openModal();
-		};
+		}
   	</script>
     <body>
 
