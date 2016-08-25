c151f28f9827b4eb5eb296a02e668c5f1af998e9
Pedro <pedrocch@fanbot.me>
2016-08-25 16:21:44 -0500

    *

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
 <html>
 <head>
 
-<?php require_once( "resources/html/header.php" ); ?>
+<?php // require_once( "resources/html/header.php" ); ?>
 	<title>Bienvenido a Fanbot</title>
 </head>
 
-<?php require_once( "resources/html/index.php" ); ?>
+<?php // require_once( "resources/html/index.php" ); ?>
 
 </html>
