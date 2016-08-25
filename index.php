88aa2dcfa3115b3e8b79d1ef6be0f7c0f1f77430
Pedro <pedrocch@fanbot.me>
2016-08-25 16:10:26 -0500

    *

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 	include 'resources/library/functions.php'; 
 	session_start();
 
-	getSiteInfo($_SERVER['HTTP_HOST']);
+//	getSiteInfo($_SERVER['HTTP_HOST']);
 
-	if( $_SESSION['site']['name'] != NULL ){
-			$_SESSION['fnbt']['name'] = $_SESSION['site']['name'];
-	} else{
-		if(isset($_SESSION['fnbt']['name'])){
-			unset($_SESSION['fnbt']['name']);
-		}
-	}
+//	if( $_SESSION['site']['name'] != NULL ){
+//			$_SESSION['fnbt']['name'] = $_SESSION['site']['name'];
+//	} else{
+//		if(isset($_SESSION['fnbt']['name'])){
+//			unset($_SESSION['fnbt']['name']);
+//		}
+//	}
 	$_SESSION['page'] = 0;
 	?>
 <!DOCTYPE html>
