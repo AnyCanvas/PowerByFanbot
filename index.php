cfe26138dacff7eb1e12101067df99058c17c5b0
Pedro <pedrocch@fanbot.me>
2016-08-25 16:33:47 -0500

    *

--- index.php
+++ index.php
@@ -4,23 +4,23 @@
 
 	getSiteInfo($_SERVER['HTTP_HOST']);
 
-//	if( $_SESSION['site']['name'] != NULL ){
+	if( $_SESSION['site']['name'] != NULL ){
 			$_SESSION['fnbt']['name'] = $_SESSION['site']['name'];
-//	} else{
-//		if(isset($_SESSION['fnbt']['name'])){
-//			unset($_SESSION['fnbt']['name']);
-//		}
-//	}
-//	$_SESSION['page'] = 0;
+	} else{
+		if(isset($_SESSION['fnbt']['name'])){
+			unset($_SESSION['fnbt']['name']);
+		}
+	}
+	$_SESSION['page'] = 0;
 	?>
 <!DOCTYPE html>
 <html>
 <head>
 
-<?php // require_once( "resources/html/header.php" ); ?>
+<?php  require_once( "resources/html/header.php" ); ?>
 	<title>Bienvenido a Fanbot</title>
 </head>
 
-<?php // require_once( "resources/html/index.php" ); ?>
+<?php  require_once( "resources/html/index.php" ); ?>
 
 </html>
