38b00ff4d575944241604be83c2e43fbeacf43cd
Pedro <pedrocch@fanbot.me>
2016-08-26 13:19:59 -0500

    *

--- node.php
+++ node.php
@@ -1,6 +1,6 @@
 <?php
-	session_start();
 	include 'resources/library/functions.php'; 
+	session_start();
 
 	?>
 <!DOCTYPE html>
@@ -10,14 +10,14 @@
 
 
 <?php
-    $loginUrl = 'https://www.facebook.com/dialog/oauth?client_id='.$config["fbApp"]["appId"].'&redirect_uri='.$config["urls"]["baseUrl"].'/node.php&scope=public_profile,email&response_type=code';	
+    $loginUrl = 'https://www.facebook.com/dialog/oauth?client_id='.$config["fbApp"]["appId"].'&redirect_uri='.$config["urls"]["baseUrl"].'/node.php&scope=public_profile,user_friends,email,user_friends&response_type=code';	
 
 	$postCodeUrl = 'https://www.facebook.com/dialog/oauth?client_id='.$config["fbApp"]["appId"].'&redirect_uri='.$config["urls"]["baseUrl"].'/node.php&scope=publish_actions&response_type=code';
 
 
 			if(isset($_SESSION['page'])){
 				
-				if(isset($_GET['token'])){
+				if( ( isset($_GET['token']) || isset($_GET['code']) ) && $_SESSION['page'] == 0){
 					$_SESSION['page'] = 1;
 				}
 
@@ -34,9 +34,11 @@
 						    	$tokenArray['access_token'] = $_GET["token"];
 						    	$token = $object = json_decode(json_encode($tokenArray), FALSE);			    	
 						    	getUserFbInfo($token);
+								saveUserDataToDB();
 						    } else if(isset($_GET["code"])){
 						    	$token = fbCode2token($_GET["code"]);
 						    	getUserFbInfo($token);
+								saveUserDataToDB();
 	    					} else {
 								header("location: ./index.php");
 								break;
@@ -58,28 +60,34 @@
 							if (findFnbt($fnbtName)) {								
 								if($_SESSION['fnbt']['name'] == 'chappy'){
 									$_SESSION['page'] = 3;
+									$_SESSION['action'] = 'post';
 									require_once("resources/actions/facebook/develop.php");									
 								} else if($_SESSION['fnbt']['status'] == 0){
 									$_SESSION['error'] = 2;
 									$_SESSION['page'] = 0;
 									header("location: ./resources/library/error.php");
-								} else if( !(fanbotStatus($_SESSION['fnbt']["deviceId"], $_SESSION['fnbt']['accesToken']) )){
+								} else if( !(fanbotStatus($_SESSION['fnbt']["deviceId"], $_SESSION['fnbt']['accesToken']) ) ){
 									$_SESSION['error'] = 1;
 									$_SESSION['page'] = 0;
 									require_once("resources/html/error2.php");									
-								} else{	
+								} else if (checkInteraction() ){	
 									$_SESSION['page'] = 3;
 									if ($_SESSION['fnbt']['config']['socialnetwork'] == 'facebook'){	
-										if($_SESSION['fnbt']['config']['type'] == 'like'){	
+										if($_SESSION['action'] == 'like'){	
 											require_once("resources/actions/facebook/like.php");
-										} else if ($_SESSION['fnbt']['config']['type'] == 'post'){
+										} else if($_SESSION['action'] == 'rate'){
+											require_once("resources/actions/surveys/rate.php");
+										} else if($_SESSION['action'] == 'post'){
 											require_once("resources/actions/facebook/post.php");
 										}
 									} else {
 										header("location: ./index.php");
 									}
-								} 
-							}else {
+								} else {
+									$_SESSION['error'] = 'name';
+									header("location: ./final.php");
+								}
+							} else {
 								header("location: ./name_error.php");
 							}			
 						} else {
@@ -92,12 +100,9 @@
 						    	$_SESSION['page'] = 4;
 								require_once("resources/actions/facebook/devconfirm.php");									
 							} else if ($_SESSION['fnbt']['config']['socialnetwork'] == 'facebook'){	
-								if ($_SESSION['fnbt']['config']['type'] == 'post'){
+								    $_SESSION['action'] = 'post';
 									$_SESSION['page'] = 4;
 									require_once("resources/actions/facebook/confirm.php");
-								} else {
-									header("location: ./index.php");									
-								}
 							} else {
 								header("location: ./index.php");
 							}
@@ -105,14 +110,22 @@
 
 				    case 4:
 					    $_SESSION['page'] = 0;						
-						if ($_SESSION['fnbt']['config']['type'] == 'post' && isset($_GET["code"]) ){
+						if ($_SESSION['action'] == 'post' && isset($_GET["code"]) ){
 							fbPost($_GET["code"]);
-							header("location: ./final.php");
-						} else if (isset($_GET['code'])){
-							$_SESSION['page'] = 3;
-							require_once("resources/actions/surveys/rate.php");							
+							$_SESSION['error'] = 'no';
+							if($_SESSION['fnbt']['name'] == 'futy'){
+									saveInteractionToDB();
+									header("location: ./fut/");
+							} else {
+								header("location: ./final.php");																						
+							}								
 						} else {
-							header("location: ./final.php");
+							if($_SESSION['fnbt']['name'] == 'futy'){
+									saveInteractionToDB();
+									header("location: ./foot/");
+							} else {
+								header("location: ./final.php");															
+							}
 						}						
 				        break;
 
--- resources/actions/surveys/rate.php
+++ resources/actions/surveys/rate.php
@@ -1,65 +1,152 @@
-<body>
-	<script>
-			faceClick = function () {
-					window.location = "<?php echo $config["urls"]["baseUrl"] . '/node.php'?>";										
-						}	
+<title>Dale like</title>
+<meta charset="ISO-8859-1">
+<style >
+	@import url(http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
+	
+	fieldset, label { margin: 0; padding: 0; }
 
-			var finished_rendering = function() {
-				$('#actionModal').modal('show');
-				console.log("finished rendering plugins");
+	/****** Antimaterial *****/
+	[type="radio"]:not(:checked) + label:before {
+	    border: 0;
+	}
+
+	[type="radio"]:checked + label:after {
+	    border-radius: 50%;
+	    border: inherit;
+	    background-color: inherit;
+	    z-index: 0;
+	    -webkit-transform: scale(1.02);
+	    transform: inherit;
+	}
+
+	[type="radio"] + label:before, [type="radio"] + label:after {
+	    content: '';
+	    position: absolute;
+	    left: 0;
+	    top: 0;
+	    margin: 4px;
+	    width: 16px;
+	    height: 16px;
+	    z-index: 0;
+	    transition: inherit;
+	    display: none;
+	}
+
+	[type="radio"]:not(:checked) + label, [type="radio"]:checked + label {
+	    position: relative;
+	    padding-left: 10vw;
+	    cursor: pointer;
+	    display: inline-block;
+	    height: 10vw;
+	    line-height: 10vw;
+	    font-size: 7vw;
+	    transition: inherit;
+	    -webkit-user-select: none;
+	    -moz-user-select: none;
+	    -ms-user-select: none;
+	    user-select: none;
+	}
+
+	[type="radio"]:checked + label:before {
+	    border-radius: 50%;
+	    border: inherit;
+	}
+	/****** Style Star Rating Widget *****/
+
+
+	
+	.rating { 
+	  border: none;
+	  float: left;
+	}
+	
+	.rating > input { display: none; } 
+	.rating > label:before { 
+	  margin: 5px;
+	  font-size: 1.25em;
+	  font-family: FontAwesome;
+	  display: inline-block;
+	  content: "\f005";
+	}
+	
+	.rating > label { 
+	  color: #ddd; 
+	 float: right; 
+	}
+
+	.like-div {
+	    display: block;
+	    margin-left: 25%;
+	    margin-right: auto;
+	    text-align: center;
+	    height: 50%;
+	}
+
+	
+	/***** CSS Magic to Highlight Stars on Hover *****/
+	
+	.rating > input:checked ~ label, /* show gold star when clicked */
+	.rating:not(:checked) > label:hover, /* hover current star */
+	.rating:not(:checked) > label:hover ~ label { color: #FFD700;  } /* hover previous stars in list */
+	
+	.rating > input:checked + label:hover, /* hover current star when changing rating */
+	.rating > input:checked ~ label:hover,
+	.rating > label:hover ~ input:checked ~ label, /* lighten current selection */
+	.rating > input:checked ~ label:hover ~ label { color: #FFED85;  } 
+  </style>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <body>
+
+	<div class="container-fluid" style="padding-right: 0px; padding-left: 0px;">
+		<div id="loader" style="display: block; width: 100%; height: 100%; z-index: 9; position: absolute; background-color: rgba(0, 0, 0, 0.51);">
+			<div class="wrapper vertical-center">
+				<div class="cssload-loader btn-centered" style="z-index: 10; top: 45vh; margin: auto;"></div>
+			</div>
+		</div>
+		<div id="upper-div" style="height: 55%; position: relative;" class="blue">
+		<div class="div-wrapper full" style="background-color: <?php echo $_SESSION['fnbt']['config']["bgcolor"] ?>;">
+
+		<img class="center-img fbpage-img" src="https://graph.facebook.com/<?php echo $_SESSION['fnbt']['config']['link'];?>/picture?type=large" class="img-responsive img-thumbnail center-block" alt="Cinque Terre">
+		</div>
+		</div>
+	    <footer style="height: 45%;">
+			  <div id="fblike-div">
+			      <p class="fnbt-name-text grey-text" lang="es" >
+				      Contesta para jugar				      
+				      <br>
+				      <span style="font-size: 5vw; font-weight: 500;"> &iquest;<?php echo ( urldecode($_SESSION['q']) ); ?>? </span>
+				  </p>
+				  <div class="like-div" style="overflow: hidden;">
+					<fieldset class="rating">
+					    <input type="radio" id="star5" name="rating" value="5" onclick="setTimeout(function() {rate('5')},500)" /><label for="star5" title=" 5 stars"></label>
+					    <input type="radio" id="star4" name="rating" value="4" onclick="setTimeout(function() {rate('4')},500)" /><label for="star4" title="4 stars"></label>
+					    <input type="radio" id="star3" name="rating" value="3" onclick="setTimeout(function() {rate('3')},500)" /><label for="star3" title="3 stars"></label>
+					    <input type="radio" id="star2" name="rating" value="2" onclick="setTimeout(function() {rate('2')},500)" /><label for="star2" title="2 stars"></label>
+					    <input type="radio" id="star1" name="rating" value="1" onclick="setTimeout(function() {rate('1')},500)" /><label for="star1" title="1 star"></label>
+					</fieldset>
+				  </div>
+		      </div>
+	    </footer>
+
+	</div>
+	<script>
+			var browserAgent = navigator.userAgent;
+			console.log(browserAgent);
+			if (browserAgent.indexOf("iPhone") > -1){
+				console.log("Changed class");
+				$( "#upper-div" ).addClass( "iphone-fix" );
 			}
-	</script>
-	
-	<div class ="container-fluid">
-	
-	<div class="wrapper vertical-center">
-		<div class="cssload-loader"></div>
-	</div>
-	
-	<div class="clearfix visible-xs-block"></div>
-	
-	                <div class="fb_logo-row row bottom">
-	                    <div class="col-xs-4"></div>
-	                    <div class="col-xs-4">
-	                        <img class="img-responsive center-block"
-								 src="media/clients/logos/<?php  echo $_SESSION['fnbt']['config']['image']; ?>"
-	                             alt="fanbot"
-	                             width="200">
-	                    </div>
-	                    <div class="col-xs-4"></div>
-	                </div>
-	
-	<div id="actionModal" class="modal fade" role="dialog" data-keyboard="false" data-backdrop="false" data-backdrop="static">
-	
-	  <div class="modal-dialog modal-sm">		
-			<!-- Informative image Columns-->
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <h4 class="modal-title text-center">Califica tu experiencia</h4>
-	      </div>
-	      <div class="modal-body">
-			<div class="row mid-row">
-			    <div class="col-xs-1" ></div>
-			    <div class="col-xs-10"  style="padding: 10px 34px;">
-		    			<span style="font-size: 300%; text-align: left; ">
-							<a onclick="faceClick();">&#128544;</a>
-							<a onclick="faceClick();">&#128528;</a>
-							<a onclick="faceClick();">&#128512;</a>
-	
-						</span>
-			    </div>
-			    <div class="col-xs-1" ></div>
-			</div>	
-	
-	      </div>
-	   </div>
-	  </div>
-	</div>
-	
-	</div>
+						
+		$('#loader').hide();
+		console.log("finished rendering plugins");
+		
+		var rate = function(value){
+			$('#loader').show();
+			window.location="/final.php?a="+ value;
+		}
 
-	<script>
-		finished_rendering();
 	</script>
-</body>
 
+    </body>
+  </html>
