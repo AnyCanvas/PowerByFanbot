a4b57232bcffa7fc212f448a7ea86c501b9a2cb3
Pedro <pedrocch@fanbot.me>
2016-09-08 14:05:23 -0500

    *

--- resources/html/index.php
+++ resources/html/index.php
@@ -30,7 +30,7 @@
 		    console.log('logged and authorized');
 		    var uid = response.authResponse.userID;
 		    var accessToken = response.authResponse.accessToken;
-			document.location.href = <?php fbLoginLink() ?>;	
+			document.location.href = <?php echo fbLoginLink() ?>;	
 		  } else if (response.status === 'not_authorized') {
 		    console.log('logged');
 		    ga('send', 'event', "step 0", 'facebook login status', 'logged');
