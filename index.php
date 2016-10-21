c32a6ccd41c833f294b658f5451742fc5b4536fd
Pedro <casillaschavezjuanpedro@gmail.com>
2016-10-21 18:55:36 -0500

    *

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 	session_start();
 	
 	getSiteInfo($_SERVER['HTTP_HOST']);
-	echo $_SERVER['HTTP_HOST'];
 
 	if( $_SESSION['site']['name'] != NULL ){
 			$_SESSION['fnbt']['name'] = $_SESSION['site']['name'];
