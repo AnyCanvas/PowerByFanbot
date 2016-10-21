a8eb54dd13f6bf4f2328e0d144b2427b52e1d852
Pedro <casillaschavezjuanpedro@gmail.com>
2016-10-21 18:30:08 -0500

    *

--- resources/html/name.php
+++ resources/html/name.php
@@ -55,21 +55,30 @@
   <!-- Modal Structure -->
   <div id="modal1" class="modal">
     <div class="modal-content">
-      <h4>Selecciona tu edad para continuar</h4>
+      <h4>Selecciona tu edad</h4>
 		  <div class="row">
 		    <div class="input-field col s6">
+			  <form id="ageForm">
 			  <input id="date" type="date" class="datepicker">
-		      <label class="active" for="date">Fecha de nacimiento</label>
+		      <label class="active" for="date" min="1998-21-10">Fecha de nacimiento</label>
+			  </form>
 		    </div>
 		  </div>
     </div>
     <div class="modal-footer">
-      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Continuar</a>
+      <a href="#!" onclick="checkAge();" class="waves-effect waves-green btn-flat">Continuar</a>
     </div>
   </div></script>	
 
   <script>
 	    $('#modal1').openModal();
+	    
+	    function checkAge(){
+			var validator = $("#ageForm").validate(options);
+			if (validator.form()) {
+			  $('#modal1').closeModal()
+			}		    
+	    }
   </script>
 	<?php
 }
