f2d042dda4384043723245551584fa74899c2591
Pedro <casillaschavezjuanpedro@gmail.com>
2016-11-05 11:04:57 -0600

    *

--- resources/html/login.php
+++ resources/html/login.php
@@ -20,10 +20,10 @@
 
 
 		<div id="upper-div" style="height: 45vh; width: 100vw; background-color: <?php echo $_SESSION['site']['bgcolor'] ?>;">
-		<div class="div-wrapper full login-img" style="background-image: url(./media/clients/<?php echo $_SESSION['site']['dir'] ?>/login.png); background-repeat: no-repeat; background-position: center center; background-size: auto auto;">
+		<div class="div-wrapper full login-img" style="background-image: url(./media/clients/<?php echo $_SESSION['site']['dir'] ?>/login.png); background-repeat: no-repeat; background-position: center bottom; background-size: auto auto;">
 		</div>
 		</div>
-	    <footer style="height: 55vh; width: 100vw; margin-top: 1vh; background-color: <?php echo $_SESSION['site']['bgcolor'] ?> !important;">
+	    <footer style="height: 55vh; width: 100vw; padding-top: 1vh; background-color: <?php echo $_SESSION['site']['bgcolor'] ?> !important;">
 			  <a class="waves-effect waves-light btn fb-btn btn-centered" style=" background-color: #405A9F; font-size: 3vw;" onclick="postclick();"><i class="mdi mdi-facebook left" style=" font-size: 4vw !important;"></i>Continuar con Facebook</a>
 			  <p style="font-size: x-small; text-align: center; padding-top: 5px;">Al continuar estarás aceptando los <a href="http://<?php echo $_SERVER['HTTP_HOST'] ?>/tyc/<?php echo $_SESSION['site']['dir'] ?>.html" target="_blank"> Términos y condiciones y el Aviso de Privacidad</a></p>
 	    </footer>
