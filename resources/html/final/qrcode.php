37a470cd8dbc802c54771174167be9e51cb7ea4d
Pedro <casillaschavezjuanpedro@gmail.com>
2016-11-09 19:03:53 -0600

    *

--- resources/html/final/qrcode.php
+++ resources/html/final/qrcode.php
@@ -18,9 +18,9 @@
 	<div class="container-fluid" style="height: 100%; width: 100%">
 
 <?php if( ($_SESSION['fnbt']['credit'] % 4) != 0){ ?>
-		<div id="upper-div" style="height: 100%; background-color: <?php echo $_SESSION['site']['bgcolor'] ?>;">
+		<div id="upper-div" style="height: 100%; background-color: "white";">
 		<div id="qrcode" class="centered" style="width:300px; height:300px; margin-top:15px; margin: auto; padding-top: 10vh;"></div>
-
+    <p>Escanea este código para canjear premio</p>
 	<a onclick="showModal();" class="waves-effect waves-light btn white-text btn-centered red darken-3" style=" bottom: 0; position: absolute; width: 100vw; left: 0vw;"><i class="material-icons left">arrow_forward</i>SOY EL ENCARGADO</a>
 		</div>
 	</div>
